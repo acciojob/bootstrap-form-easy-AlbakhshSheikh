8ce7503afe601356f06479f5de4faf76e3ce5df0
Albakhsh Sheikh <98633969+AlbakhshSheikh@users.noreply.github.com>
2025-04-23 22:56:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-(function () {
-    'use strict'
-    var forms = document.querySelectorAll('.needs-validation')
-    Array.prototype.slice.call(forms).forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-            if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-            }
-            form.classList.add('was-validated')
-        }, false)
-    })
-})()
+// script.js
+(() => {
+  'use strict';
+
+  // Fetch the form we want to apply custom validation to
+  const form = document.querySelector('.needs-validation');
+
+  // Prevent submission if form is invalid
+  form.addEventListener('submit', event => {
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    form.classList.add('was-validated');
+  }, false);
+})();
